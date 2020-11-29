09f779d8135e20a5239b2dfe2b9a3894b0d178ea
Michael Hirsch <scivision@users.noreply.github.com>
2020-11-29 12:28:41 -0500

    update kissnet cmake

--- cmake/kissnet.cmake
+++ cmake/kissnet.cmake
@@ -1,13 +1,9 @@
 include(FetchContent)
 
-# set(build_tests OFF CACHE INTERNAL "kissnet tests")
-# necessary because kissnet has option(... ON)
-
 FetchContent_Declare(kissnet
-  # GIT_REPOSITORY https://github.com/Ybalrid/kissnet.git
-  GIT_REPOSITORY https://github.com/scivision/kissnet.git
+  GIT_REPOSITORY https://github.com/Ybalrid/kissnet.git
   GIT_TAG master
-  CMAKE_ARGS "-Dbuild_tests:BOOL=off"
+  CMAKE_ARGS "-Dkissnet_BUILD_TESTING:BOOL=off"
 )
 
 FetchContent_MakeAvailable(kissnet)
