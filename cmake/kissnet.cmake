9796f0137f77d37993ac0fd9d5092a2c01041e8e
Michael Hirsch <scivision@users.noreply.github.com>
2025-07-17 22:49:48 -0400

    correct HDF5 check and pin kissnet commit hash

--- cmake/kissnet.cmake
+++ cmake/kissnet.cmake
@@ -1,9 +1,11 @@
 include(FetchContent)
 
+set(kissnet_BUILD_TESTING off)
+
 FetchContent_Declare(kissnet
   GIT_REPOSITORY https://github.com/Ybalrid/kissnet.git
-  GIT_TAG master
-  CMAKE_ARGS "-Dkissnet_BUILD_TESTING:BOOL=off"
+  GIT_TAG d37730a8fbe71a3cb6537190fe3f53eea4070602
+  GIT_SHALLOW true
 )
 
 FetchContent_MakeAvailable(kissnet)
